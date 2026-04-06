df38884debeab3f601d7cf07b949a03fe85deec6
zeneliacerejo <zenelia5e06@gmail.com>
2026-04-06 14:12:57 +0530

    Introduction to Python

---
+++ Activity1.py
@@ -0,0 +1,9 @@
+#printing a greeting message
+print ("Hello,World!")
+#printing my name
+print("My name is Zenelia.")
+#printing a fun fact about myself
+#print("I love playing chess.")
+print ("I love playing chess.")
+#preffering and printing a simple calculation
+print("The result of 3+5 is:",3+5)
